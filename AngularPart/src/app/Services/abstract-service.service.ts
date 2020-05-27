a9e0859ffe69962c1770b1866744cbbe05474527
Mihailo Jankov <mihailo.jankov.17@singimail.rs>
2020-05-27 20:41:58 +0200

    Ostatak kontrolera

---
+++ AngularPart/src/app/Services/abstract-service.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class AbstractServiceService <D, adresaDeo>{
+
+  adresaFakultet:string = "http://localhost:8080/";
+
+
+  constructor(public http:HttpClient) { }
+
+  dobavi():Observable<Fakultet[]>{
+    return this.http.get<Fakultet[]>(this.adresaFakultet);
+  }
+  dobaviPoId(id):Observable<Fakultet>{
+    return this.http.get<Fakultet>(this.adresaFakultet + "/" + id);
+  }
+
+  dodaj(obj):Observable<Fakultet>{
+    return this.http.post<Fakultet>(this.adresaFakultet, obj) ;
+  }
+
+  izmeni(obj):Observable<Fakultet>{
+    return this.http.put<Fakultet>(this.adresaFakultet + "/" + obj.id, obj);
+  }
+
+  obrisi(id):Observable<Fakultet>{
+    return this.http.delete<Fakultet>(this.adresaFakultet + "/" + id);
+  }
+}
